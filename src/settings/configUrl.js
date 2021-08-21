0a6736bc916f25abb3d5eb823a5afbe67f86647d
Han Nguyen <hannguyen3111999@gmail.com>
2021-08-21 20:06:14 +0700

    fix url

--- src/settings/configUrl.js
+++ src/settings/configUrl.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:8000";
 // export const BASE_URL_ADMIN = "http://localhost:8000/api/admin";
-export const BASE_URL = "https://smartphone-admin.000webhostapp.com/";
+export const BASE_URL = "https://smartphone-admin.000webhostapp.com";
 export const BASE_URL_ADMIN = "https://smartphone-admin.000webhostapp.com/api/admin";
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
 export const TIMESTAMP = 'TIMESTAMP';
