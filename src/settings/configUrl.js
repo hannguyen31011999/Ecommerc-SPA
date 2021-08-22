83deacd400fd766d8138920deafd9fda13fbcdf1
Han Nguyen <hannguyen3111999@gmail.com>
2021-08-22 09:07:09 +0700

    fix url

--- src/settings/configUrl.js
+++ src/settings/configUrl.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://localhost:8000";
-export const BASE_URL_ADMIN = "http://localhost:8000/api/admin";
-// export const BASE_URL = "https://smartphone-admin.000webhostapp.com";
-// export const BASE_URL_ADMIN = "https://smartphone-admin.000webhostapp.com/api/admin";
+// export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL_ADMIN = "http://localhost:8000/api/admin";
+export const BASE_URL = "https://quan9-admin.000webhostapp.com";
+export const BASE_URL_ADMIN = "https://quan9-admin.000webhostapp.com/api/admin";
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
 export const TIMESTAMP = 'TIMESTAMP';
 export const HEADER_BEARER = { headers: { "Authorization": `Bearer ${localStorage.getItem(ACCESS_TOKEN)}` } }
